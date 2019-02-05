f48f1a35b671503bdbd82db63c02817d4257f506
maoknox <femauro@gmail.com>
2019-02-05 08:06:47 -0500

    prueba deploy en heroku

--- protected/config/main.php
+++ protected/config/main.php
@@ -56,7 +56,7 @@ return array(
 //		'db'=>array(
 //			'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
 //		),
-		// uncomment the following to use a MySQL database
+		// uncomment the following to use a MySQL database No es mysql es postgresql
 		
 		'db'=>array(
                     'tablePrefix'=>'',
